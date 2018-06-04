c273fa4cab4cd819d60d2e606ddcaa81699bd525
ychalier <yohan.chalier@telecom-paristech.fr>
2018-06-04 14:34:54 +0200

    cleanup

--- beautify.py
+++ beautify.py
@@ -4,56 +4,65 @@ import csv
 import os
 import ast
 
-if not len(sys.argv) == 2 or not os.path.isfile(sys.argv[1]):
-    print("usage:\n\tpython beautify.py params.out")
-    exit()
 
-filename = sys.argv[1]
-file = open(filename, 'r')
+def load(filename):
+    file = open(filename, 'r')
+    data = {
+        'params': [],
+        'scores': []
+    }
+    for line in file.readlines():
+        if len(line) > 0:
+            tmp = line.replace('\n', '').split('\t')
+            if len(tmp) == 2:
+                params = ast.literal_eval(tmp[0])
+                scores = ast.literal_eval(tmp[1].replace('nan', '"nan"'))
+                data['params'].append(params)
+                data['scores'].append(scores)
+    file.close()
+    return data
 
-data = {
-    'params': [],
-    'scores': []
-}
 
-for line in file.readlines():
-    if len(line) > 0:
-        tmp = line.replace('\n', '').split('\t')
-        if len(tmp) == 2:
-            params = ast.literal_eval(tmp[0])
-            scores = ast.literal_eval(tmp[1].replace('nan', '"nan"'))
-            data['params'].append(params)
-            data['scores'].append(scores)
-file.close()
+def exportto_json(data, filename):
+    with open("{0}.json".format(filename[:-4]), 'w') as outfile:
+        json.dump(data, outfile)
 
-# print(json.dumps(data, indent=4, separators=(',', ': ')))
 
-# save data to a valid json file
-with open("{0}.json".format(filename[:-4]), 'w') as outfile:
-    json.dump(data, outfile)
+def exportto_csv(data, filename):
+    estimators = {}
+    for i, params_set in  enumerate(data["params"]):
+        if params_set["estimator"] not in estimators.keys():
+            estimators[params_set["estimator"]] = []
+        estimators[params_set["estimator"]].append(i)
 
-estimators = {}
-for i, params_set in  enumerate(data["params"]):
-    if params_set["estimator"] not in estimators.keys():
-        estimators[params_set["estimator"]] = []
-    estimators[params_set["estimator"]].append(i)
+    def write_to_csv(filename, keys, data, test):
+        csv_file = open(filename, 'w', newline='')
+        csv_writer = csv.writer(csv_file, delimiter=';')
+        csv_writer.writerow(['id'] + keys)
+        for i, row in enumerate(data):
+            if test(i):
+                csv_writer.writerow([i] + [row[key] for key in keys])
+        csv_file.close()
 
-def write_to_csv(filename, keys, data, test):
-    csv_file = open(filename, 'w', newline='')
-    csv_writer = csv.writer(csv_file, delimiter=';')
-    csv_writer.writerow(['id'] + keys)
-    for i, row in enumerate(data):
-        if test(i):
-            csv_writer.writerow([i] + [row[key] for key in keys])
-    csv_file.close()
+    write_to_csv('{0}.scores.csv'.format(filename[:-4]),\
+                 list(data['scores'][0].keys()),\
+                 data['scores'],\
+                 lambda x: True)
 
-write_to_csv('{0}.scores.csv'.format(filename[:-4]),\
-             list(data['scores'][0].keys()),\
-             data['scores'],\
-             lambda x: True)
+    for estimator in estimators.keys():
+        write_to_csv('{0}.params.{1}.csv'.format(filename[:-4], estimator),\
+            list(data['params'][estimators[estimator][0]].keys()),\
+            data['params'],\
+            lambda x: x in estimators[estimator])
 
-for estimator in estimators.keys():
-    write_to_csv('{0}.params.{1}.csv'.format(filename[:-4], estimator),\
-        list(data['params'][estimators[estimator][0]].keys()),\
-        data['params'],\
-        lambda x: x in estimators[estimator])
+if __name__ == '__main__':
+
+    if not len(sys.argv) == 2 or not os.path.isfile(sys.argv[1]):
+        print("usage:\n\tpython beautify.py params.out")
+        exit()
+
+
+    filename = sys.argv[1]
+    data = load(filename)
+    exportto_json(data, filename)
+    exportto_csv(data, filename)
--- parameters.py
+++ parameters.py
@@ -14,15 +14,22 @@ from sklearn import neural_network
 from detector import *
 from loader import load_detector
 
-import os
 import glob
+import os
+import sys
 
 
 def scorer(estimator, X, y):
+    """
+        A custom scoring function passed as argument to the GridSearchCV.
+        It serves two purposes:
+         - use f1 as ranking score
+         - log all tests (call to function 'write')
+    """
     t_start = time.time()
     ypred = estimator.predict(X)
     params = estimator.get_params(deep=True)
-    params['estimator'] = type(estimator).__name__
+    params['estimator'] = type(estimator).__name__  # saving estimator name
     matrix = metrics.confusion_matrix(y, ypred)
     tpr, fpr, tnr, fnr, ppv, f1 = confusion_ratios(matrix)
     scores = {
@@ -32,21 +39,28 @@ def scorer(estimator, X, y):
         'fnr': fnr,
         'ppv': ppv,
         'f1': f1,
-        't': time.time() - t_start
+        't': time.time() - t_start  # saving how much time it took to predict
     }
     write(scores, params)
     return f1
 
 
 def write(scores, params):
-    global filename
-    pid = os.getpid()
-
+    """
+        Each process spawned by GridSearchCV calls this function when it needs
+        to log the test it just performed. It logs them into a dedicated file,
+        one for each process. This avoids merge conflicts, as we do not control
+        the processing within scikit-learn.
+    """
+    global filename  # contains the timestamp, same for all processes
+    pid = os.getpid()  # process PID is used to differentiates each file
     custom_filename = '{0}.{1}.out.tmp'.format(filename, pid)
 
+    # initializing the file at first
     if not os.path.isfile(custom_filename):
         open(custom_filename, 'w').close()
 
+    # appending tests: params \t scores \n
     file = open(custom_filename, 'a')
     file.write("{0}\t{1}\n".format(params, scores))
     file.close()
@@ -58,37 +72,54 @@ filename = "params-{0}".format(timestamp)
 
 if __name__ == "__main__":
 
-    detector = load_detector()
+    """
+        We want to let the possibility to manually interrupt the execution if
+        its taking too long, without loosing the data gathered at this point.
+        This checks allows user to re-use the script later one just to merge
+        the temporary .out.tmp files.
+    """
+    if not (len(sys.argv) > 1 and sys.argv[1] == "merge"):
 
-    tests = [
-        ('MLP', neural_network.MLPClassifier(max_iter=500), {
-            'learning_rate': ['constant', 'invscaling', 'adaptive'],
-            'hidden_layer_sizes': [[p] * k for k in range(5) for p in range(1, 50)],
-            'alpha': 10.0 ** - np.arange(4, 7),
-            'activation': ['identity', 'logistic', 'tanh', 'relu'],
-            'solver': ['lbfgs', 'sgd', 'adam']
-        }),
-        ('AdaBoost', ensemble.AdaBoostClassifier(), {
-            'base_estimator': [tree.DecisionTreeClassifier(max_depth=k)\
-                                for k in range(2, 10, 2)],
-            'n_estimators': [i for i in range(1, 100, 1)],
-            'learning_rate': [x * .1 for x in range(1, 10)]
-        })
-    ]
+        detector = load_detector()
 
-    tests_min = [
-        ('AdaBoost', ensemble.AdaBoostClassifier(), {
-            'n_estimators': [i for i in range(1, 10, 1)],
-            'learning_rate': [x * .1 for x in range(1, 2)]
-        })
-    ]
+        # add the tests you want to perform here
+        tests = [
+            ('MLP', neural_network.MLPClassifier(max_iter=500), {
+                'learning_rate': ['constant', 'invscaling', 'adaptive'],
+                'hidden_layer_sizes': [[p] * k for k in range(5) for p in range(1, 50)],
+                'alpha': 10.0 ** - np.arange(4, 7),
+                'activation': ['identity', 'logistic', 'tanh', 'relu'],
+                'solver': ['lbfgs', 'sgd', 'adam']
+            }),
+            ('AdaBoost', ensemble.AdaBoostClassifier(), {
+                'base_estimator': [tree.DecisionTreeClassifier(max_depth=k)\
+                                    for k in range(2, 10, 1)],
+                'n_estimators': [i for i in range(1, 100, 1)],
+                'learning_rate': [x * .1 for x in range(1, 10)]
+            })
+        ]
 
-    for title, base_clf, parameters in tests:
-        print(title)
-        best_clf, best_params = detector.tune_parameters(base_clf, parameters,\
-                                                         verbose=10,\
-                                                         n_jobs=mp.cpu_count(),\
-                                                         scoring=scorer)
+        # used to debug this script
+        tests_minified = [
+            ('AdaBoost', ensemble.AdaBoostClassifier(), {
+                'n_estimators': [i for i in range(1, 10, 1)],
+                'learning_rate': [x * .1 for x in range(1, 2)]
+            }),
+            ('MLP', neural_network.MLPClassifier(max_iter=500), {
+                'hidden_layer_sizes': [[3] * k for k in range(5)],
+            })
+        ]
+
+        for title, base_clf, parameters in tests:
+            print(title)
+            result = detector.tune_parameters(base_clf, parameters,\
+                                              verbose=10,\
+                                              n_jobs=mp.cpu_count(),\
+                                              scoring=scorer)
+            best_clf, best_params = result
+            print('best params:\t', best_params)
+
+    # merge all *.out.tmp files into one .out file
     out_file = open("{0}.out".format(filename), 'w')
     for tmp_out_file in glob.glob('*.tmp'):
         file = open(tmp_out_file, 'r')
